5206429fe1322d1b0b85569351a501bd9c41b1c3
andrews medina <andrewsmedina@gmail.com>
2011-06-07 00:42:24 -0300

    use pure css3 to pacman sprite

--- pacman.js
+++ pacman.js
@@ -1,41 +1,69 @@
 (function(){
     document.addEventListener("keydown", keydown, false);
     window.addEventListener("MozOrientation", firefoxDeviceOrientation, false);
-    window.addEventListener("deviceorientation", chromeDeviceOrientation, false);
+    //window.addEventListener("deviceorientation", chromeDeviceOrientation, false);
 
     function chromeDeviceOrientation(e) {
-        var pacman = document.getElementById('pacman');
-        if ( pacman.style.left.length === 0 ) {
-            pacman.style.left = "10px";
-        } else {
-            pacman.style.left = (e.gamma * 10) + "px";
+        var pacman = new PacMan();
+        
+        if ( e.gamma > 0 ) {
+            pacman.moveToRight();
         }
- 
+        if ( e.gamma < -0.07 ) {
+            pacman.moveToLeft();
+        }
     };
 
     function firefoxDeviceOrientation(e) {
-        var pacman = document.getElementById('pacman');
-        if ( pacman.style.left.length === 0 ) {
-            pacman.style.left = "10px";
-        } else {
-            pacman.style.left = (e.x * 800) + "px";
+        var pacman = new PacMan();
+        
+        if ( e.x > 0 ) {
+            pacman.moveToRight();
+        }
+        if ( e.x < -0.07 ) {
+            pacman.moveToLeft();
         }
     };
 
     function keydown(e) {
-        var pacman = document.getElementById('pacman');
+        var pacman = new PacMan();
+        
         if ( e.keyCode === 39 ) {
-            if ( pacman.style.left.length === 0 ) {
-                pacman.style.left = "10px";
-            } else {
-                pacman.style.left = ( parseInt( pacman.style.left ) + 10 ) + "px";
-            }
+            pacman.moveToRight();
         }
 
         if ( e.keyCode === 37 ) {
-             if ( pacman.style.left !== 0 ) {
-                pacman.style.left = ( parseInt( pacman.style.left ) - 10 ) + "px";
-            }
+            pacman.moveToLeft();
         }
     };
+    
+    //var coins = document.getElementsByClassName('coin');
+    var pacman = new PacMan();
+    //console.log("pac", pacman.sprite.offsetLeft);
+
+    //for (var i=0; i<coins.length; i++)
+    //    console.log(coins[i].offsetLeft)
+
+    function PacMan() {
+        this.sprite = document.getElementById('pacman');
+    };
+
+    PacMan.prototype.moveToRight = function() {
+        if ( this.sprite.style.left.length === 0 ) {
+            this.sprite.style.left = "10px";
+        } else {
+            this.sprite.style.left = ( parseInt( this.sprite.style.left ) + 10 ) + "px";
+        }
+    };
+
+    PacMan.prototype.moveToLeft = function() {
+        if ( this.sprite.style.left !== 0 ) {
+            this.sprite.style.left = ( parseInt( this.sprite.style.left ) - 10 ) + "px";
+        }
+    };
+
+    //PacMan.prototype.center = function() {
+    //    return this.sprite.
+    //}
+
 })();
